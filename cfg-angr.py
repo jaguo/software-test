d458b940d7ef803d2995629933dc69aeceb02c74
jaguo <jaguo2014@outlook.com>
2018-05-23 19:57:38 +0800

    cfg-angr

--- cfg-angr.py
+++ cfg-angr.py
@@ -32,7 +32,7 @@ def parse_json(path):
 if __name__ == "__main__":
     proj = angr.Project("./demo", load_options={'auto_load_libs':False})
     main = proj.loader.main_bin.get_symbol("main")
-    
+
     count = parse_json("out.json")
 
     addr = main.addr
